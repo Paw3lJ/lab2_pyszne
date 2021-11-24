6cc2a2852438f68cc25b554f2422ceebcf3b2b6d
PaulPerez306 <PaulPerez306@gmail.com>
2021-11-24 10:34:09 +0100

    zatwierdz

--- strona.php
+++ strona.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Nie pyszne</title>
-        <meta name="description" content="Strona służąca do zamawiania jedzonka">
+        <meta name="description" content="Strona służąca do zamawiania jedzenia pod wskazany adres">
         <link rel="stylesheet" href="main.css">
     </head>
     <body>
